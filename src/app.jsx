7c9bcac825504c532ce6cb6694d2a78bd51c82cc
Jônatas Oliveira da Silva <oliveirajonatas@outlook.com.br>
2024-12-10 11:05:45 -0300

    adiciona o arquivo app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Albums from "../src/components/albums/albums";
+
+function App() {
+  return (
+    <>
+      <Albums />
+    </>
+  );
+}
+
+export default App;
